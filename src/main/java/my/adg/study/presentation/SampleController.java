0f25aa0458c659fb08d228376f33d2a487402933
deokgyun <micron6594@naver.com>
2024-11-08 17:29:51 +0900

    setting up git

--- src/main/java/my/adg/study/presentation/SampleController.java
+++ src/main/java/my/adg/study/presentation/SampleController.java
@@ -3,10 +3,12 @@ package my.adg.study.presentation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/sample")
 public class SampleController {
 
     @GetMapping("/hello")
